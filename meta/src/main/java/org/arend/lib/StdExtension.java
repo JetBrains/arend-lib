f5ae16733c05570194ee83f0177d1fdc909126e3
valis <valery.isaev@gmail.com>
2020-01-24 22:09:05 +0300

    Replace some transport with rewrite

--- meta/src/main/java/org/arend/lib/StdExtension.java
+++ meta/src/main/java/org/arend/lib/StdExtension.java
@@ -4,31 +4,22 @@ import org.arend.ext.ArendExtension;
 import org.arend.ext.DefinitionContributor;
 import org.arend.ext.DefinitionProvider;
 import org.arend.ext.concrete.ConcreteFactory;
-import org.arend.ext.concrete.expr.ConcreteExpression;
 import org.arend.ext.core.definition.CoreFunctionDefinition;
-import org.arend.ext.core.expr.CoreExpression;
-import org.arend.ext.core.expr.CoreFunCallExpression;
-import org.arend.ext.core.ops.CMP;
-import org.arend.ext.error.TypeMismatchError;
-import org.arend.ext.error.TypecheckingError;
 import org.arend.ext.module.LongName;
 import org.arend.ext.module.ModulePath;
 import org.arend.ext.module.ModuleScopeProvider;
-import org.arend.ext.prettyprinting.doc.DocFactory;
-import org.arend.ext.reference.ArendRef;
 import org.arend.ext.reference.Precedence;
 import org.arend.ext.typechecking.*;
+import org.arend.lib.tactic.RewriteTactic;
 
 import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
-import java.util.Collections;
 
 @SuppressWarnings("unused")
 public class StdExtension implements ArendExtension {
-  private ConcreteFactory factory;
-  private ModuleScopeProvider moduleScopeProvider;
+  public ConcreteFactory factory;
+  public ModuleScopeProvider moduleScopeProvider;
 
-  private CoreFunctionDefinition transport;
+  public CoreFunctionDefinition transport;
 
   @Override
   public void setConcreteFactory(@Nonnull ConcreteFactory factory) {
@@ -53,50 +44,9 @@ public class StdExtension implements ArendExtension {
 
   @Override
   public void declareDefinitions(DefinitionContributor contributor) {
-    contributor.declare(ModulePath.fromString("Paths.Meta"), LongName.fromString("rewrite"), Precedence.DEFAULT, new DeferredMetaDefinition(new BaseMetaDefinition() {
-      @Override
-      protected boolean[] argumentExplicitness() {
-        return new boolean[] { true, true };
-      }
-
-      @Override
-      public CheckedExpression invoke(@Nonnull ExpressionTypechecker typechecker, @Nonnull ContextData contextData) {
-        ConcreteExpression arg0 = contextData.getArguments().get(0).getExpression();
-        CheckedExpression path = typechecker.typecheck(arg0);
-        if (path == null) {
-          return null;
-        }
-        CoreFunCallExpression eq = path.getType().toEquality();
-        if (eq == null) {
-          typechecker.getErrorReporter().report(new TypeMismatchError(DocFactory.text("_ = _"), path.getType(), arg0));
-          return null;
-        }
-
-        CoreExpression right = eq.getDefCallArguments().get(2);
-        CoreExpression type = contextData.getExpectedType();
-        ConcreteExpression refExpr = contextData.getReferenceExpression();
-        factory.withData(refExpr.getData());
-        ArendRef ref = factory.local("x");
-        return typechecker.typecheck(
-          factory.ref(transport.getRef())
-            .app(factory.lam(Collections.singletonList(factory.param(ref)), factory.meta("transport (\\lam x => {!}) _ _", new MetaDefinition() {
-              @Nullable
-              @Override
-              public CheckedExpression invoke(@Nonnull ExpressionTypechecker typechecker, @Nonnull ContextData contextData) {
-                CheckedExpression var = typechecker.typecheck(factory.ref(ref));
-                assert var != null;
-                CoreExpression absExpr = type.recreate(expression ->
-                  typechecker.compare(expression, right, CMP.EQ, refExpr) ? var.getExpression() : null);
-                if (absExpr == null) {
-                  typechecker.getErrorReporter().report(new TypecheckingError("Cannot substitute expression", contextData.getReferenceExpression()));
-                  return null;
-                }
-                return typechecker.check(absExpr);
-              }
-            })))
-            .app(factory.core("transport _ {!} _", path))
-            .app(contextData.getArguments().get(1).getExpression()));
-      }
-    }));
+    MetaDefinition meta = new RewriteTactic(this);
+    contributor.declare(ModulePath.fromString("Paths.Meta"), LongName.fromString("rewrite"), Precedence.DEFAULT, new DeferredMetaDefinition(ExpressionTypechecker.Stage.AFTER_SOLVER, meta));
+    contributor.declare(ModulePath.fromString("Paths.Meta"), LongName.fromString("rewrite_after"), Precedence.DEFAULT, new DeferredMetaDefinition(ExpressionTypechecker.Stage.AFTER_SOLVER, meta));
+    contributor.declare(ModulePath.fromString("Paths.Meta"), LongName.fromString("rewrite_before"), Precedence.DEFAULT, new DeferredMetaDefinition(ExpressionTypechecker.Stage.BEFORE_SOLVER, meta));
   }
 }
---
+++ meta/src/main/java/org/arend/lib/tactic/RewriteTactic.java
@@ -0,0 +1,104 @@
+package org.arend.lib.tactic;
+
+import org.arend.ext.concrete.ConcreteFactory;
+import org.arend.ext.concrete.expr.*;
+import org.arend.ext.core.definition.CoreFunctionDefinition;
+import org.arend.ext.core.expr.CoreExpression;
+import org.arend.ext.core.expr.CoreFunCallExpression;
+import org.arend.ext.core.ops.CMP;
+import org.arend.ext.error.ErrorReporter;
+import org.arend.ext.error.TypeMismatchError;
+import org.arend.ext.error.TypecheckingError;
+import org.arend.ext.prettyprinting.doc.DocFactory;
+import org.arend.ext.reference.ArendRef;
+import org.arend.ext.typechecking.*;
+import org.arend.lib.StdExtension;
+
+import javax.annotation.Nonnull;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class RewriteTactic extends BaseMetaDefinition {
+  private final StdExtension ext;
+
+  public RewriteTactic(StdExtension ext) {
+    this.ext = ext;
+  }
+
+  @Override
+  protected boolean[] argumentExplicitness() {
+    return new boolean[] { false, true, true };
+  }
+
+  private void getNumber(ConcreteExpression expression, Set<Integer> result, ErrorReporter errorReporter) {
+    if (expression instanceof ConcreteNumberExpression) {
+      result.add(((ConcreteNumberExpression) expression).getNumber().intValue());
+    } else {
+      errorReporter.report(new TypecheckingError("Expected a number", expression));
+    }
+  }
+
+  @Override
+  public CheckedExpression invoke(@Nonnull ExpressionTypechecker typechecker, @Nonnull ContextData contextData) {
+    List<? extends ConcreteArgument> args = contextData.getArguments();
+    int argNum = 0;
+    Set<Integer> occurrences;
+    if (!args.get(0).isExplicit()) {
+      occurrences = new HashSet<>();
+      if (args.get(0) instanceof ConcreteTupleExpression) {
+        for (ConcreteExpression expr : ((ConcreteTupleExpression) args.get(0)).getFields()) {
+          getNumber(expr, occurrences, typechecker.getErrorReporter());
+        }
+      } else {
+        getNumber(args.get(0).getExpression(), occurrences, typechecker.getErrorReporter());
+      }
+      argNum++;
+    } else {
+      occurrences = null;
+    }
+
+    ConcreteExpression arg0 = args.get(argNum).getExpression();
+    CheckedExpression path = typechecker.typecheck(arg0);
+    if (path == null) {
+      return null;
+    }
+    CoreFunCallExpression eq = path.getType().toEquality();
+    if (eq == null) {
+      typechecker.getErrorReporter().report(new TypeMismatchError(DocFactory.text("_ = _"), path.getType(), arg0));
+      return null;
+    }
+
+    CoreExpression right = eq.getDefCallArguments().get(2);
+    CoreExpression type = contextData.getExpectedType();
+    ConcreteReferenceExpression refExpr = contextData.getReferenceExpression();
+    ConcreteFactory factory = ext.factory.withData(refExpr.getData());
+    ArendRef ref = factory.local("x");
+    return typechecker.typecheck(
+      factory.ref(ext.transport.getRef(), refExpr.getPLevel(), refExpr.getHLevel())
+        .app(factory.lam(Collections.singletonList(factory.param(ref)), factory.meta("transport (\\lam x => {!}) _ _", new MetaDefinition() {
+          @Override
+          public CheckedExpression invoke(@Nonnull ExpressionTypechecker typechecker, @Nonnull ContextData contextData) {
+            CheckedExpression var = typechecker.typecheck(factory.ref(ref));
+            assert var != null;
+            final int[] num = { 0 };
+            CoreExpression absExpr = type.recreate(expression -> {
+              if (typechecker.compare(expression, right, CMP.EQ, refExpr)) {
+                num[0]++;
+                return occurrences == null || occurrences.contains(num[0]) ? var.getExpression() : null;
+              } else {
+                return null;
+              }
+            });
+            if (absExpr == null) {
+              typechecker.getErrorReporter().report(new TypecheckingError("Cannot substitute expression", contextData.getReferenceExpression()));
+              return null;
+            }
+            return typechecker.check(absExpr);
+          }
+        })))
+        .app(factory.core("transport _ {!} _", path))
+        .app(args.get(argNum + 1).getExpression()));
+  }
+}
