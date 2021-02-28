ea1936b24b182c80d2be1119d3c2196642c4a4f1
valis <valery.isaev@gmail.com>
2021-02-28 19:25:46 +0300

    1.6.0 -> 1.6.1

--- meta/build.gradle.kts
+++ meta/build.gradle.kts
@@ -8,7 +8,7 @@ tasks.register<JavaExec>("cliCheck") {
     group = "verification"
     dependsOn(projectArend.task(":cli:jarDep"), tasks.named("classes"))
     main = "-jar"
-    val jarDepPath = projectArend.projectDir.resolve("cli/build/libs/cli-1.6.0-full.jar").absolutePath
+    val jarDepPath = projectArend.projectDir.resolve("cli/build/libs/cli-1.6.1-full.jar").absolutePath
     args(jarDepPath, "-tcr")
     workingDir(projectDir.parent)
 }
