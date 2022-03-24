e6f74b0cbf55665034fa0588b591c640564cbcc2
valis <valery.isaev@gmail.com>
2022-03-24 23:35:08 +0300

    Improve unfold metas

--- meta/src/main/java/org/arend/lib/meta/UnfoldLetMeta.java
+++ meta/src/main/java/org/arend/lib/meta/UnfoldLetMeta.java
@@ -38,7 +38,7 @@ public class UnfoldLetMeta extends BaseMetaDefinition {
       if (arg == null) {
         return null;
       }
-      return arg.replaceType(arg.getType().normalize(NormalizationMode.RNF).unfold(Collections.emptySet(), null, true, false));
+      return typechecker.replaceType(arg, arg.getType().normalize(NormalizationMode.RNF).unfold(Collections.emptySet(), null, true, false), contextData.getMarker());
     }
   }
 }
